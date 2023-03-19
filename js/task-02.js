14c84ef27597830af7ff4b0bd21313c0db6af128
Олександр Пуговський <poohovskiy@gmail.com>
2023-03-19 08:35:19 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListEl = document.querySelector('#ingredients');
+const listItemsEl = [];
+
+ingredients.forEach(ingredient => {
+  const ingredientItemEl = document.createElement('li');
+  ingredientItemEl.classList.add('item');
+  ingredientItemEl.textContent = ingredient;
+  listItemsEl.push(ingredientItemEl);
+});
+
+ingredientsListEl.append(...listItemsEl);
